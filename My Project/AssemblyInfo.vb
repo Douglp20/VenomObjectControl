d8461705b497455de6bfeeb32b03ebaa89e9516e
Douglp20 <philmore.douglas@virginmedia.com>
2020-09-24 21:29:38 +0100

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VenomObjectControl")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VenomObjectControl")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d8bff57b-3755-4044-89ac-49733e9ad7f2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.1")>
+<Assembly: AssemblyFileVersion("1.0.0.1")>
---
+++ ObjectControl.vb
@@ -0,0 +1,185 @@
+﻿Public Class ObjectControl
+
+    Public Event ErrorMessage(ByVal errDesc As String, ByVal errNo As Integer, ByVal errTrace As String)
+    Public Sub New()
+    End Sub
+    Public Function KeyPressNumeric(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) As Boolean
+        On Error GoTo Err
+
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+                KeyPressNumeric = True
+            End If
+        End If
+
+        Exit Function
+
+Err:
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+
+    End Function
+    Public Function KeyPressMoney(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) As Boolean
+        On Error GoTo Err
+
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) <> 46 Then
+                If (Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57) Then
+                    e.Handled = True
+                    KeyPressMoney = True
+                End If
+            End If
+        End If
+
+        Exit Function
+
+Err:
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+
+    End Function
+
+    Public Sub FullCapitalise(textbox As System.Windows.Forms.TextBox)
+        On Error GoTo Err
+
+        Dim txt = textbox.Text
+        Dim len = textbox.Text.ToString().Length
+        If len > 0 Then
+            Dim rst As String = textbox.Text.ToString.ToUpper()
+            textbox.Text = rst
+            textbox.SelectionStart = len
+        End If
+        Exit Sub
+
+Err:
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+    End Sub
+
+
+    Public Sub FirstCapitalise(textbox As System.Windows.Forms.TextBox)
+        On Error GoTo Err
+
+        Dim txt = textbox.Text
+        Dim len As Integer = textbox.Text.ToString().Length
+        Dim first_letter As String = ""
+        Dim other_letter As String = ""
+        Dim result As String = ""
+
+        If len > 1 Then
+            first_letter = txt.Substring(0, 1).ToUpper()
+            other_letter = txt.Substring(1, len - 1).ToLower()
+            result = first_letter.ToString() + other_letter.ToString()
+        Else
+            result = txt.Substring(0, 1).ToUpper()
+        End If
+        textbox.Text = result
+        textbox.SelectionStart = len
+        Exit Sub
+
+Err:
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+    End Sub
+    Public Sub Initialise(Sourcetextbox As System.Windows.Forms.TextBox, Destextbox As System.Windows.Forms.TextBox)
+        On Error GoTo Err
+
+        Dim txt = Sourcetextbox.Text
+        Dim len As Integer = Sourcetextbox.Text.ToString().Length
+
+        Dim result As String = String.Empty
+        Dim arrsplit() As String = txt.Split(" ")
+        Dim splitCount As Integer = arrsplit.Count
+        Dim myInitials As String = String.Empty
+
+        If splitCount = 1 Then
+            result = txt.Substring(0, 1)
+        ElseIf splitCount = 2 Then
+            result = txt.Substring(0, 1) & txt.Split(" ")(1).Substring(0, 1)
+        End If
+        Destextbox.Text = result
+
+        Exit Sub
+
+Err:
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+    End Sub
+
+    Public Sub TimeInterval(StartTime As DateTime, Interval As Integer, cbo As System.Windows.Forms.ComboBox)
+        On Error GoTo Err
+        'time
+        Dim EndTime As DateTime = "#23:30:00 PM#"
+
+        cbo.Items.Clear()
+
+
+        For i As Integer = 0 To 150
+            cbo.Items.Add(StartTime.ToShortTimeString())
+            StartTime = DateAdd(DateInterval.Minute, Interval, StartTime)
+            If StartTime = EndTime Then Exit For
+        Next
+
+
+        Exit Sub
+
+Err:
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+    End Sub
+
+    Public Function AddTimeToDate(StartDate As DateTime, time As String) As DateTime
+        On Error GoTo Err
+        'time 11:00
+        Dim strDateTime As String = CStr(Mid(StartDate, 1, 10))
+        Dim strHour As String = CStr(Mid(time, 1, 2))
+        Dim strMin As String = CStr(Mid(time, 4, 2))
+        Dim lngHour As Integer = CInt(Mid(time, 1, 2))
+
+
+        If lngHour >= 12 Then
+            strDateTime = "#" + strDateTime + " " + strHour + ":" + strMin + ":00 PM#"
+
+        Else
+            strDateTime = "#" + strDateTime + " " + strHour + ":" + strMin + ":00 AM#"
+        End If
+
+
+        AddTimeToDate = strDateTime
+
+        Exit Function
+
+Err:
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+    End Function
+    Public Function EmailCheck(ByVal strValue As String) As Boolean
+        'check the email is valid
+
+        On Error GoTo Err
+
+
+        Return strValue.Contains("@")
+
+
+        Exit Function
+
+Err:
+        Dim rtn As String = "The error occur with " + strValue + " within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+    End Function
+    Public Function WWWCheck(ByVal strValue As String) As Boolean
+        'check the Web site is valid
+
+        On Error GoTo Err
+
+        Return strValue.Contains(".com") Or strValue.Contains(".uk")
+
+        Exit Function
+
+Err:
+        Dim rtn As String = "The error occur with " + strValue + " within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+    End Function
+End Class
